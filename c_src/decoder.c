4833268a9dab17212773874fdcb3a085745d2fa2
Alex Xu <xiaobin@firevale.com>
2015-12-08 18:09:35 +0800

    wip

--- c_src/decoder.c
+++ c_src/decoder.c
@@ -633,7 +633,9 @@ is_atom_string(ErlNifBinary* bin)
     }
 
     char c;
-    for (int i=0; i < bin->size; i++) {
+    int i;
+
+    for (i=0; i < bin->size; i++) {
         c = bin->data[i];
         if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '@' || c == '_' || c == '-')
             continue;
